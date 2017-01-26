ec04edf7e612606e3c32777312ec567faed8a6cd
kaizhang33 <kaizhang33@creditease.cn>
2017-01-26 19:41:50 +0800

    v0.0.1 done

--- site.hs
+++ site.hs
@@ -1,58 +1,73 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+import Data.List (intercalate)
 import           Data.Monoid         (mappend)
 import qualified Data.Set            as S
 import           Hakyll
+import           System.FilePath     (takeBaseName, takeDirectory)
 import           Text.Pandoc.Options
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyllWith config $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/**" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "dist/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "js/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ customPandocCompiler
-            >>= loadAndApplyTemplate "templates/base.html" siteCtx
-            >>= relativizeUrls
+  categories <- buildCategories docsGlob (fromCapture "categories/*.html")
 
-    match "content/*" $ do
-        route $ setExtension "html"
-        compile $ customPandocCompiler
-            >>= loadAndApplyTemplate "templates/base.html" siteCtx
-            >>= relativizeUrls
+  match (fromList ["about.rst", "contact.markdown"]) $ do
+    route   $ setExtension "html"
+    compile $
+      customPandocCompiler
+        >>= loadAndApplyTemplate "templates/base.html" (siteCtxWithCategories categories)
+        >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            articles <- recentFirst =<< loadAll "content/*"
-            let indexCtx =
-                    listField "articles" siteCtx (return articles) `mappend`
-                    constField "title" "首页" `mappend`
-                    siteCtx
+  match docsGlob $ do
+    route $ setExtension "html"
+    compile $
+      customPandocCompiler
+        >>= loadAndApplyTemplate "templates/base.html" (siteCtxWithCategories categories)
+        >>= relativizeUrls
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/base.html" indexCtx
-                >>= relativizeUrls
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      articles <- recentFirst =<< loadAll "docs/*"
+      let indexCtx =
+            listField "articles" siteCtx (return articles) `mappend`
+            constField "title" "首页" `mappend`
+            -- categoryField "categories" categories `mappend`
+            siteCtxWithCategories categories
 
-    match "templates/**" $ compile templateCompiler
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/base.html" indexCtx
+        >>= relativizeUrls
+
+  match "templates/**" $ compile templateCompiler
 
 
 --------------------------------------------------------------------------------
 config :: Configuration
 config = defaultConfiguration { previewHost = "0.0.0.0" }
 
+loadWriteOptions :: IO WriterOptions
+loadWriteOptions = do
+  pandocTemplate <- readFile "src/pandoc-template.html"
+  return defaultHakyllWriterOptions { writerTableOfContents = True
+                                     , writerTemplate = Just pandocTemplate
+                                     }
+
 customPandocCompiler :: Compiler (Item String)
-customPandocCompiler =
-  pandocCompilerWith readerOptions defaultHakyllWriterOptions
+customPandocCompiler = do
+  writerOptions <- unsafeCompiler loadWriteOptions
+  pandocCompilerWith readerOptions writerOptions
   where customExtensions = [Ext_east_asian_line_breaks]
         defaultExtensions = readerExtensions defaultHakyllReaderOptions
         newExtensions = foldr S.insert defaultExtensions customExtensions
@@ -66,4 +81,25 @@ postCtx =
 
 siteCtx :: Context String
 siteCtx =
+  field "currentCategory"
+    (return . takeBaseName . takeDirectory . toFilePath . itemIdentifier) `mappend`
   defaultContext
+
+-- categoryCtx :: Context (String, [(String, String)])
+categoryCtx =
+  field "category" (return . fst . itemBody) `mappend`
+  listFieldWith "articles"
+    (field "title" (return . takeBaseName . toFilePath . itemBody) `mappend`
+      field "url" (fmap (maybe "" toUrl) . getRoute . itemBody))
+    (mapM makeItem . snd . itemBody)
+
+siteCtxWithCategories :: Tags -> Context String
+siteCtxWithCategories categories =
+  listField "categories" categoryCtx items `mappend`
+  siteCtx
+  where
+    tagsMapToItems = makeItem
+    items = mapM tagsMapToItems (tagsMap categories)
+
+docsGlob :: Pattern
+docsGlob = "docs/**.md"
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: './dist'
+        path: './js'
     },
     module: {
         loaders: [
